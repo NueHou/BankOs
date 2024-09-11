62ce2fdc0fc0942eb37c154ccb0509f3d06e6ba7
José Pedro <nyiuehou@gmail.com>
2024-09-11 19:57:29 -0300

    BranchUpdate

--- bank/src/main/java/com/prova/domains/Branch.java
+++ bank/src/main/java/com/prova/domains/Branch.java
@@ -7,4 +7,48 @@ public class Branch {
     private Account accounts;
     private String cnpj;
     private CnpjBranch cnpjBranch;
+
+    public Branch() {
+    }
+
+    public Branch(Integer id, String name, Account accounts, String cnpj, CnpjBranch cnpjBranch) {
+        this.id = id;
+        this.name = name;
+        this.accounts = accounts;
+        this.cnpj = cnpj;
+        this.cnpjBranch = cnpjBranch;
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Account getAccounts() {
+        return accounts;
+    }
+    public void setAccounts(Account accounts) {
+        this.accounts = accounts;
+    }
+    public String getCnpj() {
+        return cnpj;
+    }
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    public CnpjBranch getCnpjBranch() {
+        return cnpjBranch;
+    }
+    public void setCnpjBranch(CnpjBranch cnpjBranch) {
+        this.cnpjBranch = cnpjBranch;
+    }
+
+    
 }
