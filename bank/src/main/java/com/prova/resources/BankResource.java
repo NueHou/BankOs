8289724da363d7767cd93494ef8d7fd57921fb03
José Pedro <nyiuehou@gmail.com>
2024-09-15 14:03:09 -0300

    Começo de Resources

--- bank/src/main/java/com/prova/resources/BankResource.java
+++ bank/src/main/java/com/prova/resources/BankResource.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.prova.domains.Bank;
 import com.prova.dtos.BankDTO;
 import com.prova.services.BankService;
 
@@ -23,5 +25,10 @@ public class BankResource {
         return ResponseEntity.ok().body(bankService.findAll());
     }
 
-    
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<BankDTO> findById(@PathVariable Integer id){
+        Bank obj = this.bankService.findById(id);
+        return ResponseEntity.ok().body(new BankDTO(obj));
+    }
+
 }
